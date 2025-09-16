048364a213e8a489669d8a394467b519a0610c7b
EnotInternational <155220593+EnotInternational@users.noreply.github.com>
2025-09-16 09:57:22 +0300

    Telegrator bot

---
+++ MyTelegratorBot/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyTelegratorBot.Handlers;
+using Telegrator;
+
+Console.WriteLine("Hello, World!");
+
+const string BOT_TOKEN = "7605212970:AAEZVjg-CFwlpVaLlkf4bH08p6dl3vx8Pdc";
+
+var bot = new TelegratorClient(BOT_TOKEN);
+
+bot.Handlers.CollectHandlersDomainWide();
+bot.StartReceiving();
+
+Console.ReadLine();
